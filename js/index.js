53422a939de5290aadfaef2e2f0d842ba9e105a3
Qoryogdiyev <asadbekqoryogdiyev7@gmail.com>
2023-03-03 16:13:13 +0300

    open class fixed

--- js/index.js
+++ js/index.js
@@ -7,13 +7,16 @@ elButton.addEventListener("click", function () {
 
 const box1 = document.querySelector(".item__box-one");
 const box2 = document.querySelector(".item__box-two");
-
+const btn1 = document.querySelector(".item__btn-one");
+const btn2 = document.querySelector(".item__btn-two");
 const text1 = document.querySelector(".item__text-one");
 const text2 = document.querySelector(".item__text-two");
 
 box1.addEventListener("click", function () {
   text1.classList.toggle("open__text");
+  btn1.classList.toggle("open__btn");
 });
 box2.addEventListener("click", function () {
   text2.classList.toggle("open__text");
+  btn2.classList.toggle("open__btn");
 });
